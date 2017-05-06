8a82c61be185e589351267d945c488d6fc08d078
Danny Watts <dwatts3624@gmail.com>
2017-05-06 16:41:00 -0400

    Finisshed dealing with local composer modules

--- bin/drupal-json-generate.js
+++ bin/drupal-json-generate.js
@@ -3,73 +3,110 @@ const config = require('../lib/config'),
       path = require('path'),
       yaml = require('js-yaml'),
       inquirer = require('inquirer'),
-      request = require('request')
+      request = require('request-promise'),
+      console = require('console'),
+      _ = require('lodash')
       
 
-
-
 const app = {
-  //currPath = '/Users/danny/Projects/drupal-composer-builder/site'
   config: config,
   currPath: path.resolve('.'),
   appPath: '',
-  originalComposer: {},
+  composerLocal: {},
   drupalVer: '',
   init: function() {
     inquirer.prompt([{
       type: 'input',
-      name: 'appLocation',
-      message: 'Where is your app located?',
-      default: this.config.defaultDir
+      name: 'drupalLocation',
+      message: 'Where is your Drupal app located?',
+      default: this.config.drupalRoot
     }])
       .then(this.operate.bind(this))
   },
   operate: function(answers) {
-    const appPath = path.join(this.currPath, answers.appLocation)
+    const appPath = path.join(this.currPath, answers.drupalLocation)
     if (!fs.existsSync(appPath)) {
       return console.log(`${appPath} is not valid please try again with a valid directory`)
     }
     this.appPath = appPath
-    this.getOrigComposerLocal()
     
-    const drupalVer = this.originalComposer.replace['drupal/core'].replace(/[^0-9.]/g, '')
+    const composerLocal = this.getComposerLocal()
+    const drupalVer = composerLocal.replace['drupal/core'].replace(/[^0-9.]/g, '')
     this.drupalVer = drupalVer
-    console.log(drupalVer)
-    this.getOrigComposerRemote()
-      .then((data) => {
-        console.log(data)
+    
+    this.getRemoteTemplates()
+      .then((results) => {
+        const drupalDefaultModules = results.drupalDefaultComposer.require
+        const drupalLocalModules = composerLocal.require
+        // Compare local modules against remote to determine which were installed
+        const composerModules = this.shallowDiff(
+                                    drupalLocalModules, 
+                                    drupalDefaultModules)
+        const drushModules = {}
+        const modulePaths = Object.keys(this.config.modulePaths)
+        // Construct composer syntax for all currently isntalled modules
+        modulePaths.map((key, i) => {
+          const baseModulePath = this.config.modulePaths[key]
+          const modulePath = path.join(appPath, baseModulePath)
+          drushModules[modulePaths[i]] = this.getDrushModules(modulePath)
+        })
+        
+        //console.log(drushModules)
+        // Combine all known modules for composer
+        const combinedModules = _.defaults(
+                                  composerModules, 
+                                  drushModules.base,
+                                  drushModules.contrib)
+        console.log(combinedModules)
       })
       .catch((err) => {
         return console.log('Error retreiving original Drupal composer: ', err)
       })
   },
-  getOrigComposerRemote: function() {
-    const url = this.templateReplace(
-                  this.config.drupalComposerUrl, 
-                  { ver: `${this.drupalVer}.x` })
-    // Return the composer contents from the constructed URL with a promise
-    return new Promise((resolve, reject) => {
-      request(url, (err, response, body) => {
-        if(err) reject(err)
-        resolve(JSON.parse(body))
-      })
+  getRemoteTemplates: function() {
+    const templates = this.config.codeTemplates
+    const urls = Object.values(templates)
+    const replacements = { ver: `${this.drupalVer}.x` }
+    const results = {}
+    // Replace tokens in URLs with replacements object
+    urls.map((url, i) => {
+      urls[i] = this.templateReplace(url, replacements)
     })
+    // Build promises for request module with provided URLs
+    const promises = urls.map(url => request(url));
+    // Return a promise once all urls have been retrieved
+    return new Promise((resolve, reject) => {
+      Promise.all(promises)
+        .then((data) => {
+          Object.keys(templates).map((template, i) => {
+            const ext = urls[i].split('.').pop()
+            if(ext == 'json') {
+              return results[template] = JSON.parse(data[i])
+            }
+            return results[template] = data[i]
+          })
+          resolve(results)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })   
   },
-  getOrigComposerLocal: function(appPath) {
+  getComposerLocal: function(appPath) {
     appPath = this.appPath || appPath
-    const originalComposerPath = path.join(appPath,'composer.json')
-    if (!fs.existsSync(originalComposerPath)) {
-      return console.log(`${originalComposerPath} doesn't exist, please check your Drupal installation`)
+    const composerPath = path.join(appPath,'composer.json')
+    if (!fs.existsSync(composerPath)) {
+      return console.log(`${composerPath} doesn't exist, please check your Drupal installation`)
     }
-    const contents = fs.readFileSync(originalComposerPath, 'utf8')
-    return this.originalComposer = JSON.parse(contents)
+    const contents = fs.readFileSync(composerPath, 'utf8')
+    return this.composerLocal = JSON.parse(contents)
   },
-  buildComposerModules: function() {
+  getDrushModules: function(moduleDir) {
     const composerJson = {}
-    dirs(this.currPath).map(module => {
+    this.getModuleDirs(moduleDir).map(module => {
+      const configFile = `/${module}.info.yml`
+      const configPath = path.join(moduleDir, module, configFile)
       try {
-        const configFile = `/${module}.info.yml`
-        const configPath = path.join(this.currPath, module, configFile)
         if (!fs.existsSync(configPath)) return
         const contents = fs.readFileSync(configPath, 'utf8')
         // Parse YML into object
@@ -84,21 +121,33 @@ const app = {
         }
         composerJson[`drupal/${module}`] = verString
       } catch (e) {
-        console.log(e)
+        const msg = `Error parsing ${configPath}. Check the config file to `
+                  + `ensure it's properly properly formatted in YML and has a `  
+                  + `version number or install it manually when you're done:\n`
+                  + `composer require drupal/${module}\n\n`
+        console.warn(msg)
       }
     })
     return composerJson
   },
-  getModuleDirs: function(path) {
-    return fs.readdirSync(path)
-              .filter(f => fs.statSync(path.join(path, file)).isDirectory())
-              .filter(f => !(/(^|\/)\.[^\/\.]/g).test(file))
+  getModuleDirs: function(moduleDir) {
+    return fs.readdirSync(moduleDir)
+              .filter(dir => fs.statSync(path.join(moduleDir, dir)).isDirectory())
+              .filter(dir => !(/(^|\/)\.[^\/\.]/g).test(dir))
+  },
+  shallowDiff: function(a, b) {
+    const diffArr = _.difference(_.keys(a), _.keys(b))
+    const diffObj = {}
+    diffArr.map((item) => {
+      diffObj[item] = a[item]
+    })
+    return diffObj
   },
   templateReplace: function(string, replacements) {
     return string.replace(
         /{(\w*)}/g, // or /{(\w*)}/g for "{this} instead of %this%"
         ( m, key ) => {
-          return replacements.hasOwnProperty( key ) ? replacements[ key ] : '';
+          return replacements.hasOwnProperty( key ) ? replacements[ key ] : ''
         })
   } 
 }
